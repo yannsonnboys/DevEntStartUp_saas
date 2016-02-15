7e836d546e3bd322b5a5c64ce504e4abc268db43
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-15 11:05:26 +0100

    Added about page view, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  # Add a About page
+  get '/about' => 'pages#about'
+
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
