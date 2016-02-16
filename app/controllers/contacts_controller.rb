302d7db7e7c7f6473e5377ea3c8829b3c070fe21
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-16 19:02:18 +0100

    Added validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,5 +5,19 @@ class ContactsController < ApplicationController
 	end
 
 	def create
+		@contact = Contact.new(contact_params)
+
+		if @contact.save
+			flash[:success] = 'Message has been sent.'
+			redirect_to new_contact_path
+		else
+			flash[:danger] = 'Error occured. Message has not been sent, please fill all the fields.'
+			redirect_to new_contact_path
+		end
 	end
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+		end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,5 @@
 class Contact < ActiveRecord::Base
-
-
-
-
+	# check if the contact form is not empty
+	validates :name, presence: true
+	validates :email, presence: true
 end
