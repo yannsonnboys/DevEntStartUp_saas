e1de6a6bccb4a301d602d9a2ce2b043da82f9d8b
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-16 20:15:00 +0100

    Added maile and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,11 +8,22 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(contact_params)
 
 		if @contact.save
+
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			body = params[:contact][:comments]
+
+			# Method from contact_mailer.erb
+			ContactMailer.contact_email(name, email, body).deliver
+
 			flash[:success] = 'Message has been sent.'
 			redirect_to new_contact_path
+
 		else
+
 			flash[:danger] = 'Error occured. Message has not been sent, please fill all the fields.'
 			redirect_to new_contact_path
+
 		end
 	end
 
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+	# Default email
+	default to: 'y.e.s.d@icloud.com' 
+
+	def contact_mail(name, email, body)
+		@name = name
+		@email = email
+		@body = body
+
+		mail(from: email, subject: 'Contact Form Message')
+	end
+end
