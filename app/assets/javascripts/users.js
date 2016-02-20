b6f9871042d9633d44e470081bbe14eb6f7350c8
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-20 09:53:09 +0100

    Added stripe js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -33,7 +33,7 @@
 		var token = response.id;
 
 		// Add the token to the form
-		f.append('<input type="hidden" name="user[stripe_card_token]" value"' + token + '" />');
+		f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
 
 		// Submit the form
 		f.get(0).submit();
