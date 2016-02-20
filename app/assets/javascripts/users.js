e6c4942bba5913e25241960c762e1d5b2bb77252
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-20 09:50:22 +0100

    Added stripe js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,41 @@
+#(document).ready(function() {
+
+	Stripe.setPublishablekey($('meta[name="stripe-key"]').attr('content'));
+
+	// Watch for form submission
+	$("#form-submit-btn").click(function(event) {
+		event.preventDefault();
+		$('input[type=submit]').pop('disabled', true);
+
+		var error = false;
+		var ccNum = $('#card_number').val(),
+			cvcNum = $('#card_code').val(),
+			expMonth = $('#card_month').val(),
+			expYear = $('#card_year').val();
+
+		if (!error) {
+			// Get Stripe token
+			Stripe.createToken({
+				number: ccNum,
+				cvc: cvcNum,
+				exp_month: expMonth,
+				exp_year: expYear
+			}, stripeResponseHandle);
+		}
+		return false;
+	}); // Form submit
+
+	function stripeResponseHandle(status, response) {
+		// Get a reference to the form
+		var f = $("#new_user");
+
+		// Get the token from the response
+		var token = response.id;
+
+		// Add the token to the form
+		f.append('<input type="hidden" name="user[stripe_card_token]" value"' + token + '" />');
+
+		// Submit the form
+		f.get(0).submit();
+	}
+});
