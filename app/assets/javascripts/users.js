b1f70c4922d988850789b28a380d8507126798c3
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-21 03:42:30 +0100

    Replace card:stripe_card_token by source:stripe_card_token in user.rb file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,41 +1,38 @@
-#(document).ready(function() {
+$(document).ready(function() {
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
 
-	Stripe.setPublishableKey($('meta[name="stripe-key"]').attr90('content'));
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+  }); // form submission
+  
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
+    var f = $("#new_user");
 
-	// Watch for form submission
-	$("#form-submit-btn").click(function(event) {
-		event.preventDefault();
-		$('input[type=submit]').pop('disabled', true);
+    // Get the token from the response:
+    var token = response.id;
 
-		var error = false;
-		var ccNum = $('#card_number').val(),
-			cvcNum = $('#card_code').val(),
-			expMonth = $('#card_month').val(),
-			expYear = $('#card_year').val();
+    // Add the token to the form:
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
 
-		if (!error) {
-			// Get Stripe token
-			Stripe.createToken({
-				number: ccNum,
-				cvc: cvcNum,
-				exp_month: expMonth,
-				exp_year: expYear
-			}, stripeResponseHandle);
-		}
-		return false;
-	}); // Form submit
-
-	function stripeResponseHandle(status, response) {
-		// Get a reference to the form
-		var f = $("#new_user");
-
-		// Get the token from the response
-		var token = response.id;
-
-		// Add the token to the form
-		f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-
-		// Submit the form
-		f.get(0).submit();
-	}
+    // Submit the form:
+    f.get(0).submit(); 
+  }
 });
