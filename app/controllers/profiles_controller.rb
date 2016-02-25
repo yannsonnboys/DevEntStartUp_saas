0ce5c73f32c1d2bc5ec0f263f1d5d16101c6d23f
yannsonnboys <yannsonnboys01@gmail.com>
2016-02-25 15:31:55 +0100

    Added action on submit button

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,6 +23,18 @@ class ProfilesController < ApplicationController
 		@profile = @user.profile
 	end
 
+	def update
+		@user = User.find(params[:user_id])
+		@profile = @user.profile
+
+		if @profile.update_attributes(profile_params)
+			flash[:success] = "Your profile is now update!"
+			redirect_to user_path(params[:user_id])
+		else
+			render action: :edit
+		end
+	end
+
 	private
 	  def profile_params
 		params.require(:profile).permit(:first_name, :last_name, :sex, :job_title, :phone_number, :contact_email, :country, :location, :description)
