57d143bd945302f4196d94cdb55a0c8ba6da1366
yannsonnboys <yannsonnboys01@gmail.com>
2016-03-03 17:20:01 +0100

    Added project controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
     resource :profile
+    resource :projects
   end
   resources :contacts
 
